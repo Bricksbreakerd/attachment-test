593f61486ea7ef9ceef8c0d49f70df048ad61617
Bricksbreakerd <13519062532@163.com>
2026-01-18 21:26:09 +0800

    准备发布

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/attachment-test/',
+  base: '/attachment-test/', // 注意：这里必须和你的仓库名一致，前后都要有斜杠
 })
